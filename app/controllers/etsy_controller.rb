90c08ed4285c15363e92c2d3a1cd4ba8221c4860
Mike Vaughan <dvaugha1@gmail.com>
2015-02-05 18:17:15 -0500

    CSS and HTML started layout

---
+++ app/controllers/etsy_controller.rb
@@ -0,0 +1,9 @@
+class EtsyController < ApplicationController
+  def search
+    if params[:q]
+      response = Etsy::Request.get('/listings/active', :include => ['Images','Shop'], :keywords => params[:q])
+      @results = JSON.parse(response.body)['results']
+    end
+    render :search
+  end
+end
